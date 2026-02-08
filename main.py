53ad94e165c226ed88b59a996434861ff7ba70ac
ShukurluFakhri-12 <sukurovfexri21@gmail.com>
2026-02-08 10:44:23 +0400

    Add CLI support with argparse for configurable execution

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import pandas as pd
 import sqlite3
 import requests
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--limit', type=int, default=5)
+parser.add_argument('--file' , default='review.xlsx')
+parser.add_argument('--only-excel', action='store_true')
+args = parser.parse_args()
+
 
 logging.basicConfig(filename='tracker.log', level=logging.INFO, 
                     format='%(asctime)s | %(levelname)s | %(message)s')
@@ -17,7 +24,7 @@ def data_draw():
     response = requests.get(url, timeout=4)
     data = response.json()
     cleaned_set = []
-    for x in data[:5]:
+    for x in data[:args.limit]:
         new_format = {'id': x['id'], 'title': x['title'], 'link': x['body']}
         cleaned_set.append(new_format)
     logging.info(f'The number of data taken from API: {len(cleaned_set)}')
@@ -31,16 +38,17 @@ def writing_to_base(newslist):
 
 def export_to_excel():
     df = pd.read_sql('SELECT * FROM news', base)
-    df.to_excel('review.xlsx', index=False)
+    df.to_excel(args.file, index=False)
     logging.info('Excel file has been created')
 
 try:
-    received_data = data_draw()      
-    writing_to_base(received_data)  
-    export_to_excel()             
-    print("Program has been completed succesfully! Chech Log və Excel files.")
+    if not args.only_excel:
+        received_data = data_draw()      
+        writing_to_base(received_data)
+    export_to_excel()               
+    print(f"Uğurla tamamlandı! Fayl adı: {args.file}")
 except Exception as e:
-    logging.error(f"Error occurred: {e}")
+    logging.error(f"Error occurred: {e} ")
     print(f"Error: {e}")
 
 base.close()
